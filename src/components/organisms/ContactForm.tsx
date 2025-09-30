0ec96991c83d4be68b53a569f8b12cd326d1107c
Kevin Fabio Ramos López <kevinframosl@gmail.com>
2025-09-30 21:47:41 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

--- src/components/organisms/ContactForm.tsx
+++ src/components/organisms/ContactForm.tsx
@@ -2,7 +2,7 @@
 
 import { useFormState, useFormStatus } from 'react-dom';
 import { useEffect } from 'react';
-import { useToast } from '@/components/ui/use-toast';
+import { useToast } from '@/hooks/use-toast';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
